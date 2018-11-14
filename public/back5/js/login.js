6d46ca86fbaec3ec92a4c4446d11a7203252ff28
githubhanchao <hc1788797@163.com>
2018-11-14 15:10:25 +0800

    第五次登陆完成

---
+++ public/back5/js/login.js
@@ -0,0 +1,103 @@
+
+
+$(function(){
+
+  $('#form').bootstrapValidator({
+
+     //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    //3. 指定校验字段
+    fields: {
+      //校验用户名，对应name表单的name属性
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 4,
+            max: 10,
+            message: '用户名长度必须在4到10之间'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[a-zA-Z0-9_\.]+$/,
+            message: '用户名由数字字母下划线和.组成'
+          },
+          callback: {
+            message: '用户名不存在'
+          }
+        }
+      },
+      password: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 6,
+            max: 10,
+            message: '密码长度必须在6到10之间'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[a-zA-Z0-9_\.]+$/,
+            message: '密码由数字字母下划线和.组成'
+          },
+          callback: {
+            message: '密码错误'
+          }
+        }
+      }
+    }
+
+
+  })
+
+
+
+  $("#form").on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+    $.ajax({
+      type: 'post',
+      url: '/employee/employeeLogin',
+      data: $('#form').serialize(),
+      dataType: 'json', 
+      success: function(info){
+        console.log(info);
+        
+        if(info.success){
+          location.href = "index.html";
+        }
+
+        if(info.error === 1000){
+          $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
+        }
+
+        if(info.error === 1001){
+          $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
+        }
+
+
+      }
+    })
+
+  });
+
+
+  $('[type="reset"]').click(function(){
+    $('#form').data("bootstrapValidator").resetForm(true);
+  })
+
+
+})
