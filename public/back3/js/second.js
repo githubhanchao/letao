d9433b13409a6815129a0cc0b53fe6a5b8cc5f61
githubhanchao <hc1788797@163.com>
2018-11-19 11:44:09 +0800

    back3完成front3首页轮播图完成

--- public/back3/js/second.js
+++ public/back3/js/second.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   var currentPage = 1;
   var pageSize = 5;
   render();
@@ -12,16 +12,16 @@ $(function(){
         pageSize: pageSize
       },
       dataType: 'json',
-      success: function(info){
-        var htmlStr = template('secondTpl',info);
+      success: function (info) {
+        var htmlStr = template('secondTpl', info);
         $('tbody').html(htmlStr);
 
         // 进行分页初始化
         $('#paginator').bootstrapPaginator({
           bootstrapMajorVersion: 3,
-          totalPages: Math.ceil(info.total/info.size),
+          totalPages: Math.ceil(info.total / info.size),
           currentPage: info.page,
-          onPageClicked: function(a,b,c,page){
+          onPageClicked: function (a, b, c, page) {
             currentPage = page;
             render();
           }
@@ -39,118 +39,158 @@ $(function(){
 
 
 
- // 2. 点击添加按钮, 显示添加模态框
- $('#addBtn').click(function() {
+  // 2. 点击添加按钮, 显示添加模态框
+  $('#addBtn').click(function () {
 
-  // 显示添加模态框
-  $('#addModal').modal("show");
+    // 显示添加模态框
+    $('#addModal').modal("show");
 
-  // 发送ajax请求, 获取下拉菜单的列表数据(全部的一级分类)
-  // 通过分页获取一级分类的接口, 模拟获取全部数据的接口, page=1, pageSize: 100
-  $.ajax({
-    type: "get",
-    url: "/category/queryTopCategoryPaging",
-    data: {
-      page: 1,
-      pageSize: 100
-    },
-    dataType: "json",
-    success: function( info ) {
-      // console.log( info );
-      var htmlStr = template( "dropdownTpl", info );
-      $('.dropdown-menu').html( htmlStr );
-    }
+    // 发送ajax请求, 获取下拉菜单的列表数据(全部的一级分类)
+    // 通过分页获取一级分类的接口, 模拟获取全部数据的接口, page=1, pageSize: 100
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: "json",
+      success: function (info) {
+        // console.log( info );
+        var htmlStr = template("dropdownTpl", info);
+        $('.dropdown-menu').html(htmlStr);
+      }
+    })
+
+  });
+
+
+
+  // 3. 给下拉菜单的所有 a 添加点击事件, 通过事件委托注册
+  $('.dropdown-menu').on("click", "a", function () {
+    var txt = $(this).text();
+    $('#dropdownText').text(txt);
+
+    var id = $(this).data('id');
+    $('[name="categoryId"]').val(id);
+
+    $('[name="categoryId"]').trigger('input');
+
+    // 手动将 name="categoryId" 的校验状态, 改成 VALID 校验成功
+    $('#form').data("bootstrapValidator").updateStatus("categoryId", "VALID")
+
+
+
   })
 
-});
+
+  // 4. 进行文件上传初始化
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    done: function (e, data) {
+      // 后台返回的结果
+      var result = data.result;
+      // 获取文件上传的地址
+      var picUrl = result.picAddr;
+      // 设置给 img 的 src
+      $('#imgBox img').attr('src', picUrl);
+
+      // 将src路径, 实时设置给 input
+      $('[name="brandLogo"]').val(picUrl);
+
+      // 将 name="brandLogo" 的校验状态, 改成成功
+      $('#form').data('bootstrapValidator').updateStatus("brandLogo", "VALID");
 
 
-
-// 3. 给下拉菜单的所有 a 添加点击事件, 通过事件委托注册
-$('.dropdown-menu').on("click","a",function(){
-  var txt = $(this).text();
-  $('#dropdownText').text(txt);
-
-  var id = $(this).data('id');
-  $('[name="categoryId"]').val(id);
-
-  $('[name="categoryId"]').trigger('input');
-
-   // 手动将 name="categoryId" 的校验状态, 改成 VALID 校验成功
-   $('#form').data("bootstrapValidator").updateStatus("categoryId", "VALID")
-  
-
-
-})  
-
-
-// 4. 进行文件上传初始化
-$('#fileupload').fileupload({
-  dataType: 'json',
-  done: function(e,data) {
-    // 后台返回的结果
-    var result = data.result;
-    // 获取文件上传的地址
-    var picUrl = result.picAddr;
-    // 设置给 img 的 src
-    $('#imgBox img').attr('src', picUrl);
-
-    // 将src路径, 实时设置给 input
-    $('[name="brandLogo"]').val(picUrl);
-
-    // 将 name="brandLogo" 的校验状态, 改成成功
-    $('#form').data('bootstrapValidator').updateStatus("brandLogo", "VALID");
-
-
-  }
+    }
 
 
 
 
-})
+  })
 
 
 
 
 
 
-// 5. 配置表单校验
-$('#form').bootstrapValidator({
-  excluded: [],
-  // 配置校验图标
-  feedbackIcons: {
-    valid: 'glyphicon glyphicon-ok',    // 校验成功
-    invalid: 'glyphicon glyphicon-remove',  // 校验失败
-    validating: 'glyphicon glyphicon-refresh'  // 校验中
-  },
-
-  // 校验字段
-  fields: {
-    categoryId: {
-      validators: {
-        notEmpty: {
-          message: "请选择一级分类"
-        }
-      }
+  // 5. 配置表单校验
+  $('#form').bootstrapValidator({
+    excluded: [],
+    // 配置校验图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
     },
 
-    brandName: {
-      validators: {
-        notEmpty: {
-          message: "请输入二级分类名称"
+    // 校验字段
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: "请选择一级分类"
+          }
         }
-      }
-    },
+      },
 
-    brandLogo: {
-      validators: {
-        notEmpty: {
-          message: "请选择图片"
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: "请输入二级分类名称"
+          }
+        }
+      },
+
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请选择图片"
+          }
         }
       }
     }
-  }
-})
+  });
+
+
+
+  // 6. 注册表单校验成功事件, 阻止默认的表单提交, 通过 ajax 提交
+  $('#form').on('success.form.bv', function (e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: 'post',
+      url: '/category/addSecondCategory',
+      data: $('#form').serialize(),
+      success: function (info) {
+        if (info.success) {
+          $('#addModal').modal('hide');
+
+          currentPage = 1;
+          render();
+
+          // 需要重置内容 和 校验状态
+          $('#form').data('bootstrapValidator').resetForm(true);
+
+          // 由于按钮 和 图片不是表单元素, 需要手动重置
+          $('#dropdownText').text('请选择一级分类');
+          // 图片重置
+          $('#imgBox img').attr('src','./images/none.png');
+        }
+
+      }
+
+
+
+    });
+
+
+
+  });
+
+
+
+
 
 
 })
---
+++ public/front3/js/common.js
@@ -0,0 +1,11 @@
+mui('.mui-scroll-wrapper').scroll({
+  deceleration: 0.0005,
+  indicators: false
+
+});
+
+//获得slider插件对象
+var gallery = mui('.mui-slider');
+gallery.slider({
+  interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
+});
